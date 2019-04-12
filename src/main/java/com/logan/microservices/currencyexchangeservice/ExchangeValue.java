70e2bbaa739f019521eda8ea03a2bc9df689e395
Loganathan Sellappa <loganathan.sellappa@logmein.com>
2019-04-12 18:31:09 +0200

    h2 db and hardcoded data added

--- src/main/java/com/logan/microservices/currencyexchangeservice/ExchangeValue.java
+++ src/main/java/com/logan/microservices/currencyexchangeservice/ExchangeValue.java
@@ -1,20 +1,30 @@
 package com.logan.microservices.currencyexchangeservice;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@RequiredArgsConstructor()
 @NoArgsConstructor
+@Entity
 public class ExchangeValue {
 
-	private Long id;
-	private String from;
-	private String to;
-	private BigDecimal conversionMultiple;
+	@Id
+	@NonNull private Long id;
+
+	@Column(name="currency_from")
+	@NonNull	private String from;
+	@Column(name="currency_to")
+	@NonNull private String to;
+	@NonNull private BigDecimal conversionMultiple;
+
+	// Field added to return the port number of the instance which is sending response
+	// added for development purpose, not a good practice for production
+	private int port;
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(1004, 'USD', 'INR', 65, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(1005, 'EUR', 'INR', 75, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(1006, 'AUD', 'INR', 25, 0);
+
+
